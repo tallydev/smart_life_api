a70d5b6df6a63fc384698b78d9801a93f27b843e
liyijie <liyijie825@gmail.com>
2016-06-18 21:33:48 +0800

    remove unuse user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,4 @@ Rails.application.routes.draw do
   resources :banners, only: [:index]
   resource :home, only: [:show]
 
-  resources :users, only: [:create] do
-    collection do
-      post :sign_in
-    end
-  end
 end
