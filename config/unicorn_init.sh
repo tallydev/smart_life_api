dfd6829d56b2ec66ade6e331b23b2ff0f46e2ca3
liyijie <liyijie825@gmail.com>
2016-06-22 17:34:33 +0800

    unicorn user fix

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/smart/smart_life_api
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=canary
+AS_USER=smart
 set -u
 
 OLD_PIN="$PID.oldbin"
