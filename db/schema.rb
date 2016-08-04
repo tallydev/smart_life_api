27d498aec88e46c8a9800bd4fb1b11bd3a69727c
liyijie <liyijie825@gmail.com>
2016-08-04 17:08:56 +0800

    add Sport::Monthly model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804090540) do
+ActiveRecord::Schema.define(version: 20160804090640) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -147,6 +147,16 @@ ActiveRecord::Schema.define(version: 20160804090540) do
   end
 
   add_index "sport_weeklies", ["user_id"], name: "index_sport_weeklies_on_user_id"
+
+  create_table "sport_yearlies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "count"
+    t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sport_yearlies", ["user_id"], name: "index_sport_yearlies_on_user_id"
 
   create_table "sports", force: :cascade do |t|
     t.integer  "user_id"
