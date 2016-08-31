4e3880630d5759b72e4a58a157b35fa20c8f1acf
liyijie <liyijie825@gmail.com>
2016-08-31 14:59:19 +0800

    update user admin attributes

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,9 +3,10 @@ ActiveAdmin.register User do
     selectable_column
     id_column
     column :phone
-    column :nickname
+    column :name
     column :sex
     column :birth
+    column :nickname
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
