c84a9fcf9dce0879dec6b460bdd25f7267cd8b1d
liyijie <liyijie825@gmail.com>
2016-11-23 16:47:35 +0800

    add detail_image to activity sqhd

--- app/admin/activity/sqhd.rb
+++ app/admin/activity/sqhd.rb
@@ -2,12 +2,16 @@ ActiveAdmin.register Activity::Sqhd do
   # menu parent: "预约信息"
 
   permit_params :title,
-    image_attributes: [:id, :desc, :photo, :_destroy]
+    image_attributes: [:id, :desc, :photo, :_destroy],
+    detail_image_attributes: [:id, :desc, :photo, :_destroy],
 
   index do
     column :title
     column :image do |activity|
       link_to(image_tag(activity.image.photo.url(:mini)), activity.image.photo.url) if activity.image
+    end
+    column :detail_image do |activity|
+      link_to(image_tag(activity.detail_image.photo.url(:mini)), activity.detail_image.photo.url) if activity.detail_image
     end
     column :created_at
 
@@ -23,6 +27,12 @@ ActiveAdmin.register Activity::Sqhd do
           ? cf.template.content_tag(:span, "还未选择图片文件")
           : cf.template.link_to(image_tag(image.photo.url(:medium)), image.photo.url, target: "_blank")
       end
+      f.fields_for :detail_image, for: [:detail_image, f.object.detail_image || f.object.build_detail_image] do |cf|
+        detail_image = cf.object
+        cf.input :photo, as: :file, label: "活动详细图", hint: (detail_image.try(:photo).blank?) \
+          ? cf.template.content_tag(:span, "还未选择图片文件")
+          : cf.template.link_to(image_tag(detail_image.photo.url(:medium)), detail_image.photo.url, target: "_blank")
+      end
     end
 
     f.actions
@@ -34,6 +44,11 @@ ActiveAdmin.register Activity::Sqhd do
       row :image do
         if activity.image
           link_to(image_tag(activity.image.photo.url(:medium)), activity.image.photo.url, target: "_blank")
+        end
+      end
+      row :detail_image do
+        if activity.detail_image
+          link_to(image_tag(activity.detail_image.photo.url(:medium)), activity.detail_image.photo.url, target: "_blank")
         end
       end
 
