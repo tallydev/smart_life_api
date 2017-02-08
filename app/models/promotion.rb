1bf2ac6fceeecff11fab9f5b608176edbbf0069c
mumumumushu <mumumumushu@gmail.com>
2017-02-08 13:04:01 +0800

    fix destroy of promotion & add order desc to promotion

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -26,16 +26,16 @@
 
 class Promotion < Product
 	# 限量销售
-	default_scope { where( product_type: 1 ) }
+	default_scope { where( product_type: 1).order("created_at DESC") }
 
-	validate :check_expiration_time
+	# validate :check_expiration_time
 
 
 
 
 	private
 
-	def check_expiration_time
-		errors.add(:expiration_time, "到期时间错误") if self.expiration_time &&  self.expiration_time < Time.now
-	end
+	# def check_expiration_time
+	# 	errors.add(:expiration_time, "到期时间错误") if self.expiration_time &&  self.expiration_time < Time.now
+	# end
 end
